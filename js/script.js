d2868b6e8498142d62009c75a9be95b8912af54f
Arian Cordoba <arian.gasolero@gmail.com>
2021-01-23 02:24:00 -0300

    First version

--- js/script.js
+++ js/script.js
@@ -1,8 +1,58 @@
-document.getElementById("icon-menu").addEventListener("click", mostrar_menu);
+import productos from "/js/productos.js";
 
-function mostrar_menu(){
+let productsDiv = document.getElementById("productos");
 
-    document.getElementById("move-content").classList.toggle('move-container-all');
-    document.getElementById("show-menu").classList.toggle('show-lateral');
+renderProducts(productos);
 
+function renderProducts({ productos }) {
+  let html = `<div class="container-post"><div class="posts">`;
+  productos.map((producto, i, error) => {
+    html = html + productInfo(i, producto);
+  });
+  html = html + `</div></div>`;
+  productsDiv.innerHTML = html;
+}
+
+function productInfo(i, product) {
+  return `<div class="post">
+  <div class="ctn-img">
+      <img src="${product.img}" alt="">
+  </div>
+  <h2>${product.precio}</h2>
+  <p>${product.descripcion}</p>
+  <button class="btn btn-sm btn-success btnBuy" data-toggle="modal" data-target="#modal_${i}">Comprar</button>
+  <div class="modal fade" id="modal_${i}" tabindex="-1" role="dialog" aria-labelledby="fm-modal-grid" aria-hidden="true">
+      <div class="modal-dialog modal-xl">
+          <div class="modal-content">
+              <div class="modal-header">
+                  <h5 class="modal-title" id="">AcriCandy</h5>
+                  <button class="close" data-dismiss="modal" aria-label="Cerrar">
+                      <span aria-hidden="true">&times;</span>
+                  </button>
+              </div>
+
+              <div class="modal-body">
+                  <div class="container-fluid">
+                      <div class="row">
+                          <div class="col-12 col-sm-6">
+                              <img class="productImage" src="${product.img}" alt="">
+                          </div>
+                          <div class="col-12 col-sm-6">
+                              <p class="status">${product.estado}</p>
+                              <h3 class="productTitle">${product.titulo}</h3>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+
+              <div class="modal-footer">
+                  <a class="btn btn-success" target="_blank"  href="https://mercadolibre.com">Comprar en mercado libre</a>
+                  <button class="btn btn-default" data-dismiss="modal">Cerrar</button>
+              </div>
+          </div>
+      </div>
+  </div>
+  
+  <a href="#" class="btn btn-sm btn-dark btnDetalles">Detalles</a>
+</div>`;
 }
